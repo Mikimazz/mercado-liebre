2d7a6a5c0ae69b5892d47a2f5b17e10d3f84744d
Michele Mazzinghi <mmazz91@gmail.com>
2022-05-19 21:19:48 -0300

    actualización app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Estamos corriendo en el puerto" + PORT)
+});
+
 app.listen(3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
